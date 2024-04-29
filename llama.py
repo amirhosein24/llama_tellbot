d096e53a0ebb398ad03ce20a6940ec6fc0eb1401
amirhosein24 <am.heidari24@gmail.com>
2024-04-29 16:37:15 +0330

    2 models, better ui

--- llama.py
+++ llama.py
@@ -5,18 +5,46 @@ from creds import groq_token as token
 client = Groq(api_key=token)
 
 
-def ask_llama(promt):
+def ask_llama(prompt, model="llama3-8b-8192"):
 
     completion = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model=model,
         messages=[
             {
                 "role": "system",
-                "content": "helpfull assisstant"
+                "content": "you are a helpfull assisstant, answer prompts in same language as prompt"
             },
             {
                 "role": "user",
-                "content": promt
+                "content": prompt
+            }
+
+        ],
+        temperature=1,
+        max_tokens=4096,
+        top_p=1,
+        stream=True,
+        stop=None,
+    )
+    return completion
+
+
+def ask_llama_reply(prompt, reply, model="llama3-8b-8192"):
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "you are a helpfull assisstant, answer prompts in same language as prompt"
+            },
+            {
+                "role": "assistant",
+                "content": reply
+            },
+            {
+                "role": "user",
+                "content": prompt
             }
 
         ],
